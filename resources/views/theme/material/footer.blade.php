556aea5146b279a2bcdca4ea6a83de659bb84013
JohnMontoya <johnmontoya777@hotmail.com>
2019-06-13 16:10:31 -0500

    Share_Views, Layouts, Ruta para inicio

---
+++ resources/views/theme/material/footer.blade.php
@@ -0,0 +1,48 @@
+      <footer class="footer">
+        <div class="container-fluid">
+          <nav class="float-left">
+            <ul>
+              <li>
+                <a href="https://www.creative-tim.com">
+                  Creative Tim
+                </a>
+              </li>
+              <li>
+                <a href="https://creative-tim.com/presentation">
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a href="http://blog.creative-tim.com">
+                  Blog
+                </a>
+              </li>
+              <li>
+                <a href="https://www.creative-tim.com/license">
+                  Licenses
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div class="copyright float-right">
+            &copy;
+            <script>
+              document.write(new Date().getFullYear())
+            </script>, made with <i class="material-icons">favorite</i> by
+            <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a> for a better web.
+          </div>
+        </div>
+      </footer>
+  </div>
+</div>
+<!--   Core JS Files   -->
+<script src="{{asset("assets/$theme/assets/js/core/jquery.min.js")}}"></script>
+<script src="{{asset("assets/$theme/assets/js/core/popper.min.js")}}"></script>
+<script src="{{asset("assets/$theme/assets/js/core/bootstrap-material-design.min.js")}}"></script>
+<script src="{{asset("assets/$theme/assets/js/plugins/perfect-scrollbar.jquery.min.js")}}"></script>
+<!-- Plugin for the momentJs  -->
+<script src="{{asset("assets/$theme/assets/js/plugins/moment.min.js")}}"></script>
+<!--Material Dashboard -->
+<script src="{{asset("assets/$theme/assets/js/material-dashboard.js?v=2.1.1")}}" type="text/javascript"></script>
+<!-- Material Dashboard DEMO methods, don't include it in your project! -->
+<script src="{{asset("assets/$theme/assets/demo/demo.js")}}"></script>
